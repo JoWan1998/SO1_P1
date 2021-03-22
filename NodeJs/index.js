ee9b87a249f5843aeeabb99a3fc4614877db85d0
Jose Wannan <2993999310101@ingenieria.usac.edu.gt>
2021-03-21 19:55:50 -0600

    tipo infeccion optimizacion

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -207,32 +207,37 @@ function infect_(visitados){
 //Tipo infectado 
 function type_(visitados){
     const lista = []
-    const filtro=[]
+    let cantidad = 0;
     //listamos solo datos que necesitamos
     visitados.forEach(element => {
-        lista.push(element.infectedtype.toLowerCase())
+        if(element.infectedtype != null)
+        {
+            var _k= false;
+            lista.forEach(element1=>
+            {
+                if(element1.infectedtype === element.infectedtype.toLowerCase())
+                {
+                    element1.count = element1.count + 1;
+                    _k = true;
+                }
+            })
+            if(!_k) lista.push({ infectedtype: element.infectedtype.toLowerCase(), count: 1, porcent: 0});
+        }
+
     });
     //quitamos duplicados
-    const lista2 = lista.filter((item,index)=>{
-        return lista.indexOf(item) == index;
-      })
-    //sacamos el porcentaje
-    lista2.forEach(data => {
-        contador =0;
-        visitados.forEach(data2 => {
-            if(data==data2.infectedtype.toLowerCase()){
-                contador+=1
-            }
-        });
-        var porcentaje = ((contador*100)/lista.length);
-        filtro.push({
-            infectedtype: data,
-            porcent: porcentaje
-        })
-    }); 
-    return filtro;
-}
+    lista.forEach(element=>
+    {
+        cantidad += element.count;
+    })
 
+    lista.forEach(element=>
+    {
+        element.porcent = (element.count/cantidad)*100;
+    })
+
+    return lista;
+}
 //Region
 function regiM_(visitados,num){
     const lista = []
